9a89ca07ba4a583945a1aa4a6473e546f1707b41
zengqb <719725611@qq.com>
2018-07-17 09:59:40 +0800

    配置修改

---
+++ shopping-mall-provider/main/java/com/zqb/ShoppingMallMain.java
@@ -0,0 +1,25 @@
+package com.zqb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright © 2012-2050,dajia Co.,Ltd. All rights reserved.
+ * This program is protected by copyright laws;
+ * <p>
+ * Program Name: shopping-mall
+ * <p>
+ * Description:
+ * <p>
+ * Created by zengqb on 2018/7/16
+ *
+ * @author zengqb
+ * @version 1.0
+ */
+@SpringBootApplication
+public class ShoppingMallMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallMain.class, args);
+    }
+}
