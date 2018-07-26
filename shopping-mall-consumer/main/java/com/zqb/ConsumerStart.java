031bc4369eb03ee72f5c8eac94140290f7f5fb26
zengqb <719725611@qq.com>
2018-07-26 10:46:00 +0800

    接口添加

--- shopping-mall-consumer/main/java/com/zqb/ConsumerStart.java
+++ shopping-mall-consumer/main/java/com/zqb/ConsumerStart.java
@@ -1,7 +1,9 @@
 package com.zqb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Copyright © 2012-2050,dajia Co.,Ltd. All rights reserved.
@@ -17,6 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class ConsumerStart {
 
     public static void main(String[] args) {
